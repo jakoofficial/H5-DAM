5fb40386910fd87b40062e758b91a6c814bc22df
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-02-04 08:40:50 +0100

    Fixed issue with db getAll now works and has error messages

--- src/lib/DB/dbConn.js
+++ src/lib/DB/dbConn.js
@@ -3,6 +3,15 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database("test.db");
 let sql = "SELECT * FROM tester";
 
-const data = db.all(sql);
-console.log(db.get("select * from tester"));
+let data = db.all(sql, getData);
+console.log(data)
+
+// @ts-ignore
+function getData(err, rows) {
+    if (err) {
+        console.log(err.message);
+        return;
+    }
+    return rows[0];
+}
 db.close();
