df8b683d4d696bed0299a6747576d0c1e471272e
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-02-04 09:29:18 +0100

    Added a new DB function for adding a new entry into tester(will change)

--- src/lib/DB/dbConn.js
+++ src/lib/DB/dbConn.js
@@ -1,17 +1,64 @@
+// @ts-ignore
 import sqlite3 from "sqlite3";
 
 const db = new sqlite3.Database("test.db");
-let sql = "SELECT * FROM tester";
 
-let data = db.all(sql, getData);
-console.log(data)
+const COMDS = {
+    "GetAllData":"SELECT * FROM tester",
+}
 
 // @ts-ignore
-function getData(err, rows) {
-    if (err) {
-        console.log(err.message);
-        return;
-    }
-    return rows[0];
-}
+// @ts-ignore
+const getAll = async (db, sql, params) => {
+  return new Promise((resolve, reject) => {
+// @ts-ignore
+    db.all(sql, params, (err, rows) => {
+      if (err) reject(err);
+      resolve(rows);
+    });
+  });
+};
+
+// @ts-ignore
+const getFirst = async (db, sql, params) => {
+  return new Promise((resolve, reject) => {
+// @ts-ignore
+    db.get(sql, params, (err, row) => {
+      if (err) reject(err);
+      resolve(row);
+    });
+  });
+};
+
+// @ts-ignore
+const addNew = async (db, sql, params = []) => {
+  if (params && params.length > 0) {
+    return new Promise((resolve, reject) => {
+// @ts-ignore
+      db.run(sql, params, (err) => {
+        if (err) reject(err);
+        // @ts-ignore
+        resolve();
+      });
+    });
+  }
+  return new Promise((resolve, reject) => {
+// @ts-ignore
+    db.exec(sql, (err) => {
+      if (err) reject(err);
+      // @ts-ignore
+      resolve();
+    });
+  });
+};
+
+
+// @ts-ignore
+let recievedData = [];
+let sql = COMDS.GetAllData;
+
+await addNew(db, "INSERT INTO tester(Field2) VALUES(?)", ["Inserted from Code"]);
+
+// @ts-ignore
+console.log(await getAll(db, sql))
 db.close();
