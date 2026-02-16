943295fb6ba9fbf52b6ef786b0d82c4ee83dd75b
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-02-16 10:35:27 +0100

    made the sign in page more accurate

--- DAM/src/routes/signin/+page.svelte
+++ DAM/src/routes/signin/+page.svelte
@@ -1,2 +1,42 @@
 <h1>Sign in</h1>
-<p>hello</p>
+
+<div id="loginform">
+    <input type="text" placeholder="Username">
+    <input type="password" placeholder="Password">
+    <p id="errormsg">Error</p>
+    <div id="buttons">
+        <button>Sign up</button>
+        <button>Sign in</button>
+    </div>
+</div>
+
+<style>
+@media screen and (max-width: 420px) {
+
+    h1 {
+        width: 100%;
+        text-align: center;
+        margin-top: 98px;
+    }
+    #loginform {
+        display: block;
+        width: 270px;
+        margin: auto;
+    }
+
+    #loginform input {
+        width: 270px;
+        height: 40px;
+        margin-bottom: 20px;
+    }
+    #loginform #errormsg {
+        font-size: 16px;
+    }
+    #loginform #buttons button {
+        width:130px;
+        height: 46px;
+    }
+}
+@media screen and (min-width: 421px) {
+}
+</style>
