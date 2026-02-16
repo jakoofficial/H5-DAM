b1e09ac3f6af8fe312da98697b65b29e74a047df
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-02-16 10:45:27 +0100

    Fixed styling margins

--- DAM/src/routes/signin/+page.svelte
+++ DAM/src/routes/signin/+page.svelte
@@ -12,7 +12,6 @@
 
 <style>
 @media screen and (max-width: 420px) {
-
     h1 {
         width: 100%;
         text-align: center;
@@ -23,13 +22,22 @@
         width: 270px;
         margin: auto;
     }
-
     #loginform input {
         width: 270px;
         height: 40px;
+        font-size: 18px;
+        text-indent: 5px;
+    }
+    #loginform input:first-of-type {
+        margin-top: 50px;
         margin-bottom: 20px;
     }
+    #loginform input:last-of-type {
+        margin-bottom: 5px;
+    }
     #loginform #errormsg {
+        margin-bottom: 10px;
+        min-height: 50px;
         font-size: 16px;
     }
     #loginform #buttons button {
