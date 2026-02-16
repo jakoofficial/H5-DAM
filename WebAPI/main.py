9f4de72da012d8643d4c1c0644f6c18a08545449
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-02-16 11:55:09 +0100

    Made the first api call to make a group using the API

--- WebAPI/main.py
+++ WebAPI/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import sqlite3
+from objs import *
 
 app = FastAPI()
 
@@ -16,6 +18,13 @@ app.add_middleware(
 def read_root():
     return {"Hello": "World"}
 
+@app.post("/api/addnewgroup")
+def addnew_group(groupName):
+    con = sqlite3.connect("DAMDB.db")
+    cur = con.cursor()
+    cur.execute(f"INSERT INTO Groups(GroupName) Values(?)", (groupName))
+    con.commit()
+    con.close()
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str | None = None):
---
+++ WebAPI/objs.py
@@ -0,0 +1,7 @@
+from dataclasses import dataclass
+
+@dataclass
+class DAMGroup():
+    groupID: int = 0
+    groupName: str = ""
+
