2e8c9a880c6b8d70453f6c34036b14e97a079bd3
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2026-03-10 13:00:33 +0100

    sad

--- DAM/src/routes/+page.svelte
+++ DAM/src/routes/+page.svelte
@@ -1,27 +1,36 @@
 <script>
-    import { Get, GetSessionToken } from "$lib/DataFetcher";
-	import { onMount } from "svelte";
+	import { Get, GetSessionToken } from '$lib/DataFetcher';
+	import { onMount } from 'svelte';
 
-    export let username = "";
-    export let isReady = false;
-    async function getuser() {
-        // @ts-ignore
-        const dataArr = await Get('get_user', {'session-token': GetSessionToken()});
-        username = dataArr;
-    }
+	export let username = '';
+	export let isReady = false;
+	async function getuser() {
+		// @ts-ignore
+		const dataArr = await Get('get_user', { 'session-token': GetSessionToken() });
+		username = dataArr;
+	}
 
-    onMount(()=>{
-        if(GetSessionToken()){
-            getuser();
-            isReady = true;
-        }
-        else{username = ""}
-    })
+	onMount(() => {
+		if (GetSessionToken()) {
+			getuser();
+			isReady = true;
+		} else {
+			username = '';
+		}
+	});
 </script>
 
-{#if username && isReady}
-<p>Welcome <span style="text-transform: capitalize;">{username}</span></p>
-{:else}
-<h1>DAM</h1>
-{/if}
+<div id="wrapper">
+	{#if username && isReady}
+		<p>Welcome <span style="text-transform: capitalize;">{username}</span></p>
+	{:else}
+		<h1>DAM</h1>
+	{/if}
+</div>
 
+<style>
+#wrapper{
+    width: 85%;
+    margin: auto;
+}
+</style>
